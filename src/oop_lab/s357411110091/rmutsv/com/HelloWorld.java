25424e54645db9167e7b1ad5eb127cbd815bf78a
piyanuch442 <winnydog178@gmail.com>
2017-11-19 15:21:21 +0700

    Edit information

--- src/oop_lab/s357411110091/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s357411110091/rmutsv/com/HelloWorld.java
@@ -7,8 +7,7 @@ public static void main(String[] args) {
 
     System.out.println("Hello World, I'am Kie");
     System.out.println("Piyanuch Na Nakorn");
-    System.out.println("Information System");
-    System.out.println("MT: RMUTSV");
+    System.out.println("Information System, MT:RMUTSV");
 
 
 }//main
