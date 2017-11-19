b87fca02c5e407c0b3e039e584d51fedcce64a0e
piyanuch442 <winnydog178@gmail.com>
2017-11-19 14:49:09 +0700

    Testing upload to github

---
+++ src/oop_lab/s357411110091/rmutsv/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package oop_lab.s357411110091.rmutsv.com;
+
+public class HelloWorld {
+
+//    create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello World, I'am Kie");
+
+
+}//main
+
+}//class
