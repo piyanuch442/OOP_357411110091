5628d41b5ff3d7f3e60152afc336e32eaecb7a1e
piyanuch442 <winnydog178@gmail.com>
2018-02-06 11:03:39 +0700

    Has-a relation, person,address,job

--- src/oop_lab9/PersonApp.java
+++ src/oop_lab9/PersonApp.java
@@ -2,11 +2,12 @@ package oop_lab9;
 
 public class PersonApp {
     public static void main(String[] args) {
-        Address address = new Address("200/1","Nakhonsithammarat","80000");
-
-        Job job = new Job("Police",15000);
-
-        Person person = new Person("1809900701254","Tan",address, job);
+        Person person = new Person("1809900701254","Tan",
+                new Address("200/1","NST","80000"),new Job("Police",15000));
         System.out.println(person.toString());
+
+        person.getJob().setSalary(20000);
+        System.out.println(person.getJob().getSalary());
+        System.out.println(person.getAddress().getPostCode());
     }
 }//class
